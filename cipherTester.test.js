43e0ed140633f1c228709b2384f4e56f0351f171
JDC-droid <josh040890@gmail.com>
2025-05-14 19:47:23 +0100

    Add Javascript

--- cipherTester.test.js
+++ cipherTester.test.js
@@ -1,17 +1,17 @@
-const { cipherValidation } = require("./cipher.js");
+const { enDeCode } = require("./cipher.js");
 
 test("fails if the cipher is not a string", () => {
-  expect(cipherValidation(123).toBe(false));
+  expect(enDeCode(124566, { checked: true }, 5)).toBe(false);
 });
 
-test("fials if the cipher is empty", () => {
-  expect(cipherValidation("").toBe(false));
+test("fails if the cipher is empty", () => {
+  expect(enDeCode("").toBe(false));
 });
 
-test("fails if the cipher does not decrypt", () => {
-  expect(cipherValidation("1234567890").toBe(false));
-});
+// test("fails if the cipher does not decrypt", () => {
+//   expect(enDeCode("1234567890").toBe(false));
+// });
 
-test("fails if the cipher does not encrypt", () => {
-  expect(cipherValidation("1234567890").toBe(false));
-});
+// test("fails if the cipher does not encrypt", () => {
+//   expect(enDeCode("1234567890").toBe(false));
+// });
