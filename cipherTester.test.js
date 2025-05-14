0213d83567b2ba3e63d4fd2e975fd0685aaeb670
JDC-droid <josh040890@gmail.com>
2025-05-14 20:11:21 +0100

    Add passing test's

--- cipherTester.test.js
+++ cipherTester.test.js
@@ -5,13 +5,20 @@ test("fails if the cipher is not a string", () => {
 });
 
 test("fails if the cipher is empty", () => {
-  expect(enDeCode("").toBe(false));
+  //expect(enDeCode("", { checked: true }, 5)).toBe(false);
+  if (typeof enDeCode("", { checked: true }, 5) === "string") {
+    expect(enDeCode("", { checked: true }, 5)).toBe(false);
+  }
 });
 
-// test("fails if the cipher does not decrypt", () => {
-//   expect(enDeCode("1234567890").toBe(false));
-// });
+test("fails if the cipher does not decrypt", () => {
+  expect(enDeCode("rovvy dbkfovob", { checked: false }, 10)).toBe(
+    "hello traveler"
+  );
+});
 
-// test("fails if the cipher does not encrypt", () => {
-//   expect(enDeCode("1234567890").toBe(false));
-// });
+test("fails if the cipher does not encrypt", () => {
+  expect(enDeCode("hello traveler", { checked: true }, 10)).toBe(
+    "rovvy dbkfovob"
+  );
+});
